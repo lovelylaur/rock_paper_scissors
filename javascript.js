c73d85e47adb7618245b2a2a9400609c81f06334
Lauren <171189862+lovelylaur@users.noreply.github.com>
2025-01-17 16:48:58 -0500

    Add skeleton for win/lose conditions

--- javascript.js
+++ javascript.js
@@ -3,17 +3,13 @@
 // Equate the number to a specified string
 // Print the value of the string
 
+let choices = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-   return Math.floor(Math.random() * 3);
+   return choices[Math.trunc(Math.random() * choices.length)];
 }
 
-if (getComputerChoice() === 0) {
-   console.log("Computer: Rock");
-} else if (getComputerChoice() === 1) {
-   console.log("Computer: Paper");
-} else {
-   console.log("Computer: Scissors");
-}
+console.log(getComputerChoice());
 
 // When prompted to input a string
 // The user will input "rock, "paper," or "scissors"
@@ -33,3 +29,39 @@ if (getHumanChoice() === "rock") {
 } else if (getHumanChoice() === "scissors") {
    console.log("You: Scissors");
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Write a function to indicate a round played on refresh
+// When the human or computer choice variable is true, incrememnt their score value by 1
+
+function playRound(humanChoice, computerChoice) {
+   //code
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+if (computerSelection === "rock" && humanSelection === "paper") {
+   console.log("You win! Paper beats rock!");
+} else if (computerSelection === "rock" && humanSelection === "scissors") {
+   console.log("You lose! Rock beats scissors!");
+} else if (computerSelection === "paper" && humanSelection === "rock") {
+   console.log("You lose! Paper beats rock!");
+} else if (computerSelection === "paper" && humanSelection === "scissors") {
+   console.log("You win! Scissors beats paper!");
+} else if (computerSelection === "scissors" && humanSelection === "rock") {
+   console.log("You win! Rock beats scissors!");
+} else if (computerSelection === "scissors" && humanSelection === "paper") {
+   console.log("You lose! Scissors beats paper!");
+} else if (computerSelection === "rock" && humanSelection === "rock") {
+   console.log("Tie!");
+} else if (computerSelection === "paper" && humanSelection === "paper") {
+   console.log("Tie!");
+} else if (computerSelection === "scissors" && humanSelection === "scissors") {
+   console.log("Tie!");
+}
+
+
+playRound(humanSelection, computerSelection);
